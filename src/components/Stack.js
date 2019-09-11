c4a8fdafe4055a2d939b166d8069a0c5c5d08e1f
Meleesha <mayola.dsz@gmail.com>
2019-09-12 00:03:28 +0100

    code optimization

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -3,22 +3,20 @@ import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import Card from './Card';
 
-export class Stack extends Component {
-    render() {
-        const { title, cards } = this.props.stack;
-        return (
-            <div>
-                <Link className='link-home' to="/"><h4>Home</h4></Link>
-                <h1>{title}</h1>
-                <br />
-                {
-                    cards.map(card =>
-                        <Card key={card.id} card={card} />
-                    )}
-            </div>
-        )
-    }
+export const Stack = ({ stack: { title, cards } }) => {
+    return (
+        <div>
+            <Link className='link-home' to="/"><h4>Home</h4></Link>
+            <h1>{title}</h1>
+            <br />
+            {
+                cards.map(card =>
+                    <Card key={card.id} card={card} />
+                )}
+        </div>
+    )
 }
+
 
 function mapStateToProps(state) {
     return { stack: state.stack };
